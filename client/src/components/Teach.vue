7193159cabdb102607f9a28635944537c42dacfb
ic17b065 <ic17b065@technikum-wien.at>
2019-06-25 12:45:07 +0200

    Teach View erweitert
    Lehrinhalte ergänzt

--- client/src/components/Teach.vue
+++ client/src/components/Teach.vue
@@ -5,14 +5,20 @@
 
     <div class="row">
       <div class="col-md-7">
-        <h2 class="display-4">
+        <h2 class="display-4 text-center">
           Plus (+)
           <br>
-          <span class="text-muted">1+1=2</span>
+          <small>
+          <span style="color: #008000;">&nbsp;&nbsp; 15 &nbsp;&nbsp;</span>&nbsp;&nbsp; + &nbsp;&nbsp;<span style="color: #ff00ff;">&nbsp;&nbsp; 5 &nbsp;&nbsp;</span>&nbsp;&nbsp; = &nbsp;&nbsp;<span style="color: #ff0000;">&nbsp;&nbsp;20&nbsp;&nbsp;</span>
+          <br>
+          <span style="color: #008000;">Summand</span> + <span style="color: #ff00ff;">Summand</span> = <span style="color: #ff0000;">Sum</span>
+        </small>
         </h2>
         <p
           class="lead"
-        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        >Die Addition, umgangssprachlich auch Plus-Rechnen genannt, ist eine der vier Grundrechenarten. Oft sagt man statt "addieren" auch "zusammenzählen".<br>
+         Die Zahlen, die addiert werden, nennt man Summanden , den Ausdruck:<br>
+         1. Summand + 2. Summand nennt man Summe und das Ergebnis der Rechnung: Wert der Summe.</p>
       </div>
       <div class="col-md-5">
         <img
@@ -29,14 +35,21 @@
 
     <div class="row">
       <div class="col-md-7 order-md-2">
-        <h2 class="display-4">
+        <h2 class="display-4 text-center">
           Minus (-)
           <br>
-          <span class="text-muted">5-2=3</span>
+          <small>
+          <span style="color: #008000;">&nbsp;&nbsp; 13 &nbsp;&nbsp;</span>&nbsp;&nbsp; - &nbsp;&nbsp;<span style="color: #ff00ff;">&nbsp;&nbsp; 4 &nbsp;&nbsp;</span>&nbsp;&nbsp; = &nbsp;&nbsp;<span style="color: #ff0000;">&nbsp;&nbsp;9&nbsp;&nbsp;</span>
+          <br>
+          <span style="color: #008000;">Minuend</span> - <span style="color: #ff00ff;">Subtrahend</span> = <span style="color: #ff0000;">Difference</span>
+          </small>
         </h2>
         <p
           class="lead"
-        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        >Die Subtraktion ist eine der vier Grundrechenarten. In der Grundschule und in der Umgangssprache verwendet man meist den Ausdruck Abziehen für die Subtraktion von zwei oder mehr Zahlen.<br>
+        Die Zahl von welcher "abgezogen" wird nennt man Minuend, die Zahl welche "abgezogen" wird Subtrahend.<br>
+        Der Ausdruck Minuend - Subtrahend wird Differenz genannt und das Ergebnis der Rechnung Wert der Differenz.
+        </p>
       </div>
       <div class="col-md-5 order-md-1">
         <img
@@ -53,14 +66,22 @@
 
     <div class="row">
       <div class="col-md-7">
-        <h2 class="display-4">
+        <h2 class="display-4 text-center">
           Multiplication (⋅)
           <br>
-          <span class="text-muted">2⋅3=6</span>
+          <small>
+          <span style="color: #008000;">&nbsp; 5 &nbsp;</span>&nbsp; * &nbsp;<span style="color: #ff00ff;">&nbsp; 8 &nbsp;</span>&nbsp; = &nbsp;<span style="color: #ff0000;">&nbsp; 40 &nbsp;</span>
+          <br>
+          <span style="color: #008000;">Factor</span> * <span style="color: #ff00ff;">Factor</span> = <span style="color: #ff0000;">Product</span>
+          </small>
         </h2>
         <p
           class="lead"
-        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        >
+        Die Multiplikation ist eine der vier Grundrechenarten. Anstatt eine Zahl öfter zusammen zu zählen
+        kann man mit dem Mal-Nehmen schneller auf ein Ergebnis kommen und hat weniger Schreibarbeit.<br>
+        Die Multiplikation setzt sich aus dem Produkt, das ist die Rechnung und dem Wert des Produkts, das ist das Ergebnis der Rechnung zusammen. Das Produkt setzt sich aus Faktoren zusammen. Es kann zwei oder mehr Faktoren geben.
+        </p>
       </div>
       <div class="col-md-5">
         <img
@@ -77,14 +98,21 @@
 
     <div class="row">
       <div class="col-md-7 order-md-2">
-        <h2 class="display-4">
+        <h2 class="display-4 text-center">
           Division (÷)
           <br>
-          <span class="text-muted">6:3=2</span>
+          <small>
+          <span style="color: #008000;">&nbsp;&nbsp; 6 &nbsp;&nbsp;</span>&nbsp;&nbsp; : &nbsp;&nbsp;<span style="color: #ff00ff;">&nbsp;&nbsp; 2 &nbsp;&nbsp;</span>&nbsp;&nbsp; = &nbsp;&nbsp;<span style="color: #ff0000;">&nbsp;&nbsp; 3 &nbsp;&nbsp;</span>
+          <br>
+          <span style="color: #008000;">Dividend</span> : <span style="color: #ff00ff;">Divisor</span> = <span style="color: #ff0000;">Quotient</span>
+          </small>
         </h2>
         <p
           class="lead"
-        >Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        >Die Division ist eine der vier Grundrechenarten. Zum Beispiel für 6:2, spricht man 6 dividiert durch 2, oder auch 6 geteilt durch 2.<br>
+        Die Division setzt sich aus verschiedenen Begriffen zusammen.Teilt man so zum Beispiel die Zahl 10 durch 5, heißt 10 Dividend und 5 Divisor. Die Rechnung 10:5 nennt man Quotient. Das Ergebnis der Rechnung nennt man Wert des Quotienten.
+        
+        </p>
       </div>
       <div class="col-md-5 order-md-1">
         <img
